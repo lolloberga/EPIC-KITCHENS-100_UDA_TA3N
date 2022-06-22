4228c5766b49b0e9af23c6a6ce90a1470d53e3f2
lorenzo <lorenzo.bergadano@intesa.it>
2022-06-22 11:08:12 +0200

    add: modify LSTA classess

--- LSTA/ConvLSTACell.py
+++ LSTA/ConvLSTACell.py
@@ -31,36 +31,36 @@ class ConvLSTACell(nn.Module):
         self.conv_o_cam = nn.Conv2d(1, 1, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
 
         if zero_init:
-            torch.nn.init.constant(self.conv_i_s.weight, 0)
-            torch.nn.init.constant(self.conv_i_s.bias, 0)
-            torch.nn.init.constant(self.conv_i_cam.weight, 0)
+            torch.nn.init.constant_(self.conv_i_s.weight, 0)
+            torch.nn.init.constant_(self.conv_i_s.bias, 0)
+            torch.nn.init.constant_(self.conv_i_cam.weight, 0)
 
-            torch.nn.init.constant(self.conv_f_s.weight, 0)
-            torch.nn.init.constant(self.conv_f_s.bias, 0)
-            torch.nn.init.constant(self.conv_f_cam.weight, 0)
+            torch.nn.init.constant_(self.conv_f_s.weight, 0)
+            torch.nn.init.constant_(self.conv_f_s.bias, 0)
+            torch.nn.init.constant_(self.conv_f_cam.weight, 0)
 
-            torch.nn.init.constant(self.conv_a_s.weight, 0)
-            torch.nn.init.constant(self.conv_a_s.bias, 0)
+            torch.nn.init.constant_(self.conv_a_s.weight, 0)
+            torch.nn.init.constant_(self.conv_a_s.bias, 0)
 
-            torch.nn.init.constant(self.conv_o_s.weight, 0)
-            torch.nn.init.constant(self.conv_o_s.bias, 0)
-            torch.nn.init.constant(self.conv_o_cam.weight, 0)
+            torch.nn.init.constant_(self.conv_o_s.weight, 0)
+            torch.nn.init.constant_(self.conv_o_s.bias, 0)
+            torch.nn.init.constant_(self.conv_o_cam.weight, 0)
         else:
-            torch.nn.init.xavier_normal(self.conv_i_s.weight)
-            torch.nn.init.constant(self.conv_i_s.bias, 0)
-            torch.nn.init.xavier_normal(self.conv_i_cam.weight)
+            torch.nn.init.xavier_normal_(self.conv_i_s.weight)
+            torch.nn.init.constant_(self.conv_i_s.bias, 0)
+            torch.nn.init.xavier_normal_(self.conv_i_cam.weight)
 
-            torch.nn.init.xavier_normal(self.conv_f_s.weight)
-            torch.nn.init.constant(self.conv_f_s.bias, 0)
-            torch.nn.init.xavier_normal(self.conv_f_cam.weight)
+            torch.nn.init.xavier_normal_(self.conv_f_s.weight)
+            torch.nn.init.constant_(self.conv_f_s.bias, 0)
+            torch.nn.init.xavier_normal_(self.conv_f_cam.weight)
 
-            torch.nn.init.xavier_normal(self.conv_a_s.weight)
-            torch.nn.init.constant(self.conv_a_s.bias, 0)
-            torch.nn.init.xavier_normal(self.conv_a_cam.weight)
+            torch.nn.init.xavier_normal_(self.conv_a_s.weight)
+            torch.nn.init.constant_(self.conv_a_s.bias, 0)
+            torch.nn.init.xavier_normal_(self.conv_a_cam.weight)
 
-            torch.nn.init.xavier_normal(self.conv_o_s.weight)
-            torch.nn.init.constant(self.conv_o_s.bias, 0)
-            torch.nn.init.xavier_normal(self.conv_o_cam.weight)
+            torch.nn.init.xavier_normal_(self.conv_o_s.weight)
+            torch.nn.init.constant_(self.conv_o_s.bias, 0)
+            torch.nn.init.xavier_normal_(self.conv_o_cam.weight)
 
         # Memory params
 
@@ -81,38 +81,80 @@ class ConvLSTACell(nn.Module):
                                   bias=False)
 
         if zero_init:
-            torch.nn.init.constant(self.conv_i_x.weight, 0)
-            torch.nn.init.constant(self.conv_i_x.bias, 0)
-            torch.nn.init.constant(self.conv_i_c.weight, 0)
+            torch.nn.init.constant_(self.conv_i_x.weight, 0)
+            torch.nn.init.constant_(self.conv_i_x.bias, 0)
+            torch.nn.init.constant_(self.conv_i_c.weight, 0)
 
-            torch.nn.init.constant(self.conv_f_x.weight, 0)
-            torch.nn.init.constant(self.conv_f_x.bias, 0)
-            torch.nn.init.constant(self.conv_f_c.weight, 0)
+            torch.nn.init.constant_(self.conv_f_x.weight, 0)
+            torch.nn.init.constant_(self.conv_f_x.bias, 0)
+            torch.nn.init.constant_(self.conv_f_c.weight, 0)
 
-            torch.nn.init.constant(self.conv_c_x.weight, 0)
-            torch.nn.init.constant(self.conv_c_x.bias, 0)
-            torch.nn.init.constant(self.conv_c_c.weight, 0)
+            torch.nn.init.constant_(self.conv_c_x.weight, 0)
+            torch.nn.init.constant_(self.conv_c_x.bias, 0)
+            torch.nn.init.constant_(self.conv_c_c.weight, 0)
 
-            torch.nn.init.constant(self.conv_o_x.weight, 0)
-            torch.nn.init.constant(self.conv_o_x.bias, 0)
-            torch.nn.init.constant(self.conv_o_c.weight, 0)
+            torch.nn.init.constant_(self.conv_o_x.weight, 0)
+            torch.nn.init.constant_(self.conv_o_x.bias, 0)
+            torch.nn.init.constant_(self.conv_o_c.weight, 0)
         else:
-            torch.nn.init.xavier_normal(self.conv_i_x.weight)
-            torch.nn.init.constant(self.conv_i_x.bias, 0)
-            torch.nn.init.xavier_normal(self.conv_i_c.weight)
+            torch.nn.init.xavier_normal_(self.conv_i_x.weight)
+            torch.nn.init.constant_(self.conv_i_x.bias, 0)
+            torch.nn.init.xavier_normal_(self.conv_i_c.weight)
 
-            torch.nn.init.xavier_normal(self.conv_f_x.weight)
-            torch.nn.init.constant(self.conv_f_x.bias, 0)
-            torch.nn.init.xavier_normal(self.conv_f_c.weight)
+            torch.nn.init.xavier_normal_(self.conv_f_x.weight)
+            torch.nn.init.constant_(self.conv_f_x.bias, 0)
+            torch.nn.init.xavier_normal_(self.conv_f_c.weight)
 
-            torch.nn.init.xavier_normal(self.conv_c_x.weight)
-            torch.nn.init.constant(self.conv_c_x.bias, 0)
-            torch.nn.init.xavier_normal(self.conv_c_c.weight)
+            torch.nn.init.xavier_normal_(self.conv_c_x.weight)
+            torch.nn.init.constant_(self.conv_c_x.bias, 0)
+            torch.nn.init.xavier_normal_(self.conv_c_c.weight)
 
-            torch.nn.init.xavier_normal(self.conv_o_x.weight)
-            torch.nn.init.constant(self.conv_o_x.bias, 0)
-            torch.nn.init.xavier_normal(self.conv_o_c.weight)
+            torch.nn.init.xavier_normal_(self.conv_o_x.weight)
+            torch.nn.init.constant_(self.conv_o_x.bias, 0)
+            torch.nn.init.xavier_normal_(self.conv_o_c.weight)
 
+    def forward(self, x, state_att, state_inp, x_flow_i=0, x_flow_f=0, x_flow_c=0, x_flow_o=0):
+        # state_att = [a, s]
+        # state_inp = [atanh(c), o]
+
+        a_t_1 = state_att[0]
+        s_t_1 = state_att[1]
+
+        c_t_1 = F.tanh(state_inp[0])
+        o_t_1 = state_inp[1]
+
+        # Attention recurrence
+
+        i_s = F.sigmoid(self.conv_i_s(s_t_1))
+        f_s = F.sigmoid(self.conv_f_s(s_t_1))
+        o_s = F.sigmoid(self.conv_o_s(s_t_1))
+        a_tilde = F.tanh(self.conv_a_s(s_t_1))
+        a = (f_s * a_t_1) + (i_s * a_tilde)
+        s = o_s * F.tanh(a)
+        u = s  # hidden state + cam
+
+        u = F.softmax(u.view(u.size(0), -1), 1)
+        u = u.view(u.size(0), 1, 7, 7)
+
+        x_att = x * u.expand_as(x)
+
+        i_x = F.sigmoid(self.conv_i_c(o_t_1 * c_t_1) + self.conv_i_x(x_att) + x_flow_i)
+        f_x = F.sigmoid(self.conv_f_c(o_t_1 * c_t_1) + self.conv_f_x(x_att) + x_flow_f)
+        c_tilde = F.tanh(self.conv_c_c(o_t_1 * c_t_1) + self.conv_c_x(x_att) + x_flow_c)
+        c = (f_x * state_inp[0]) + (i_x * c_tilde)
+
+        c_vec = self.avgpool(c).view(c.size(0), -1)
+        c_logits = self.c_classifier(c_vec) + self.coupling_fc(self.avgpool(x_att).view(x_att.size(0), -1))
+        c_probs, c_idxs = c_logits.sort(1, True)
+        c_class_idx = c_idxs[:, 0]
+        c_cam = self.c_classifier.weight[c_class_idx].unsqueeze(2).unsqueeze(2) * c
+        o_x = F.sigmoid(self.conv_o_x(o_t_1 * c_t_1) + self.conv_o_c(c_cam))
+
+        state_att = [a, s]
+        state_inp = [c, o_x]
+        return state_att, state_inp, x_att
+
+    '''
     def forward(self, x, cam, state_att, state_inp, x_flow_i=0, x_flow_f=0, x_flow_c=0, x_flow_o=0):
         # state_att = [a, s]
         # state_inp = [atanh(c), o]
@@ -153,3 +195,4 @@ class ConvLSTACell(nn.Module):
         state_att = [a, s]
         state_inp = [c, o_x]
         return state_att, state_inp, x_att
+    '''
--- LSTA/attentionModule.py
+++ LSTA/attentionModule.py
@@ -1,34 +1,50 @@
 from torch.autograd import Variable
 from LSTA.ConvLSTACell import *
 from LSTA import resNet
+from tqdm import tqdm
 
 
 class attentionModel(nn.Module):
     def __init__(self, num_classes=51, mem_size=512, c_cam_classes=1000):
         super(attentionModel, self).__init__()
         self.num_classes = num_classes
-        self.resNet = resNet.resnet34(True, True)
+        #self.resNet = resNet.resnet34(True, True)
         self.mem_size = mem_size
-        self.lsta_cell = ConvLSTACell(512, mem_size, c_cam_classes)
+        self.lsta_cell = ConvLSTACell(2048, mem_size, c_cam_classes)
         self.avgpool = nn.AvgPool2d(7)
         self.dropout = nn.Dropout(0.7)
         self.fc = nn.Linear(mem_size, self.num_classes)
         self.classifier = nn.Sequential(self.dropout, self.fc)
         #todo: implementare ulteriori livelli della rete (Sudakaran email)
 
-    def forward(self, inputVariable):
-        state_att = (Variable(torch.zeros(inputVariable.size(1), 1, 7, 7).cpu()),
-                     Variable(torch.zeros(inputVariable.size(1), 1, 7, 7).cpu()))
-        state_inp = (Variable(torch.zeros((inputVariable.size(1), self.mem_size, 7, 7)).cpu()),
-                 Variable(torch.zeros((inputVariable.size(1), self.mem_size, 7, 7)).cpu()))
-        for t in range(inputVariable.size(0)):
-            logit, feature_conv, x = self.resNet(inputVariable[t])
-            bz, nc, h, w = feature_conv.size()
-            feature_conv1 = feature_conv.view(bz, nc, h * w)
-            probs, idxs = logit.sort(1, True)
-            class_idx = idxs[:, 0]
-            cam = torch.bmm(self.resNet.fc.weight[class_idx].unsqueeze(1), feature_conv1).view(x.size(0), 1, 7, 7)
-            state_att, state_inp, _ = self.lsta_cell(x, cam, state_att, state_inp)
+        # static params use from external methods
+        self.loss_fn            = None
+        self.optimizer_fn       = None
+        self.optim_scheduler    = None
+
+    def forward(self, features):
+        # Features = Tensor (32, 5, 2048, 7, 7)
+        state_att = (Variable(torch.zeros(features.size(1), 1, 7, 7).cpu()),
+                     Variable(torch.zeros(features.size(1), 1, 7, 7).cpu()))
+        state_inp = (Variable(torch.zeros((features.size(1), self.mem_size, 7, 7)).cpu()),
+                     Variable(torch.zeros((features.size(1), self.mem_size, 7, 7)).cpu()))
+
+        for t in tqdm(range(features.size(0))):
+            features_reshaped = features[t, :, :, :, :]
+            state_att, state_inp, _ = self.lsta_cell(features_reshaped, state_att, state_inp)
+
         feats = self.avgpool(state_inp[0]).view(state_inp[0].size(0), -1)
         logits = self.classifier(feats)
+        feats = feats.unsqueeze(1).repeat(1, 5, 1) # In order to mantains featurs as TA3N wants
         return logits, feats
+
+    # General utils
+    def set_loss_fn(self, loss):
+        self.loss_fn = loss
+    def set_optimizer_fn(self, optimizer):
+        self.optimizer_fn = optimizer
+    def set_optim_scheduler(self, optim_scheduler):
+        self.optim_scheduler = optim_scheduler
+
+# Noi partiamo direttamente da feature_conv riga 25 (dati da Chiara)
+# Noi abbiamo (B, 5, 7, 7, 1024)... Come trattano la dimensione temporale? for t in ... ?
