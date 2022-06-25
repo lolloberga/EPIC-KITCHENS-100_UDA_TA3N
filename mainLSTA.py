2986c68facb6a923ffeb5dc70b1339644cc3e1a9
lorenzo <lorenzo.bergadano@intesa.it>
2022-06-25 16:29:42 +0200

    add: write report files

--- mainLSTA.py
+++ mainLSTA.py
@@ -102,9 +102,27 @@ def main():
     if not os.path.isdir(path_exp):
         os.makedirs(path_exp)
 
-    if args.tensorboard:
-        writer_train = SummaryWriter(path_exp + '/tensorboard_train')  # for tensorboardX
-        writer_val = SummaryWriter(path_exp + '/tensorboard_val')  # for tensorboardX
+    note_fl = open(path_exp + '/note.txt', 'w')
+    note_fl.write('Number of Epochs = {}\n'
+                  'lr = {}\n'
+                  'Train Batch Size = {}\n'
+                  'Sequence Length = {}\n'
+                  'Decay steps = {}\n'
+                  'Decay factor = {}\n'
+                  'Memory size = {}\n'
+                  'Memory cam classes = {}\n'.format(args.epochs, args.lr, args.batch_size, args.num_segments, args.lr_steps, args.lr_decay,
+                                                     args.mem_size, args.outPool_size))
+
+    note_fl.close()
+
+    # Log files
+    writer = SummaryWriter(path_exp)
+    train_log_loss = open((path_exp + '/train_log_loss.txt'), 'w')
+    train_log_acc = open((path_exp + '/train_log_acc.txt'), 'w')
+    train_log_loss_batch = open((path_exp + '/train_log_loss_batch.txt'), 'w')
+    test_log_loss = open((path_exp + '/test_log_loss.txt'), 'w')
+    test_log_acc = open((path_exp + '/test_log_acc.txt'), 'w')
+
     # === initialize the model ===#
     print(Fore.CYAN + 'preparing the model......')
 
@@ -127,7 +145,6 @@ def main():
     # calculate the number of videos to load for training in each list ==> make sure the iteration # of source & target are same
     num_source = len(pd.read_pickle(args.train_source_list).index)
     num_target = len(pd.read_pickle(args.train_target_list).index)
-    num_val = len(pd.read_pickle(args.val_list).index)
 
     num_iter_source = num_source / args.batch_size[0]
     num_iter_target = num_target / args.batch_size[1]
@@ -148,6 +165,8 @@ def main():
                             test_mode=True
                             )
 
+    print(Fore.CYAN + 'Number of train samples = {}'.format(source_set.__len__()))
+
     source_sampler = torch.utils.data.sampler.RandomSampler(source_set)
     source_loader = torch.utils.data.DataLoader(source_set, batch_size=args.batch_size[0], shuffle=False,
                                                 sampler=source_sampler, num_workers=args.workers, pin_memory=True)
@@ -164,6 +183,8 @@ def main():
                             random_shift=False,
                             test_mode=True,
                             )
+
+    print(Fore.CYAN + 'Number of test samples = {}'.format(target_set.__len__()))
 
     target_sampler = torch.utils.data.sampler.RandomSampler(target_set)
     target_loader = torch.utils.data.DataLoader(target_set, batch_size=args.batch_size[1], shuffle=False,
@@ -208,6 +229,7 @@ def main():
         trainSamples = 0
         iterPerEpoch = 0
         model.classifier.train(True)
+        writer.add_scalar('lr', optimizer_fn.param_groups[0]['lr'], epoch + 1)
         data_loader = enumerate(zip(source_loader, target_loader))
 
         for i, ((source_data, source_label, source_id), (target_data, target_label, target_id)) in data_loader:
@@ -230,13 +252,19 @@ def main():
                 line = 'Training loss after {} iterations = {} '.format(train_iter, loss_value)
                 print(line)
                 result_file.write(line + '\n')
+                train_log_loss_batch.write('Training loss after {} iterations = {}\n'.format(train_iter, loss_value))
+                writer.add_scalar('train/iter_loss', loss_value, train_iter)
         avg_loss = epoch_loss / iterPerEpoch
         trainAccuracy = (numCorrTrain / trainSamples) * 100
-        line_avg = 'Average training loss after {} epoch = {} '.format(epoch + 1, avg_loss)
-        line_acc = 'Training accuracy after {} epoch = {}% '.format(epoch + 1, trainAccuracy)
+        line_avg = 'Average training loss after {} epoch = {} '.format(epoch, avg_loss)
+        line_acc = 'Training accuracy after {} epoch = {}% '.format(epoch, trainAccuracy)
         print(line_avg)
         print(line_acc)
         result_file.write(line_avg + '\n' + line_acc + '\n')
+        writer.add_scalar('train/epoch_loss', avg_loss, epoch + 1)
+        writer.add_scalar('train/accuracy', trainAccuracy, epoch + 1)
+        train_log_loss.write('Training loss after {} epoch = {}\n'.format(epoch + 1, avg_loss))
+        train_log_acc.write(line_acc)
 
         if epoch % args.eval_freq == 0 or epoch == args.epochs:
             print('Testing...')
@@ -246,7 +274,6 @@ def main():
             test_samples = 0
             numCorr = 0
             for i, (val_data, val_label, _) in enumerate(target_loader):
-                #print('testing inst = {}'.format(i))
                 test_iter += 1
                 test_samples += val_data.size(0)
                 valdataVariable = val_data.permute(1, 0, 2, 3, 4)
@@ -257,11 +284,15 @@ def main():
                 numCorr += (predicted == val_label.to(dev)).sum()
             test_accuracy = (numCorr / test_samples) * 100
             avg_test_loss = test_loss_epoch / test_iter
-            line_avg = 'Test Loss after {} epochs, loss = {}'.format(epoch + 1, avg_test_loss)
-            line_acc = 'Test Accuracy after {} epochs = {}%'.format(epoch + 1, test_accuracy)
+            line_avg = 'Test Loss after {} epochs, loss = {}'.format(epoch, avg_test_loss)
+            line_acc = 'Test Accuracy after {} epochs = {}%'.format(epoch, test_accuracy)
             print(line_avg)
             print(line_acc)
             result_file.write(line_avg + '\n' + line_acc + '\n')
+            writer.add_scalar('test/epoch_loss', avg_test_loss, epoch + 1)
+            writer.add_scalar('test/accuracy', test_accuracy, epoch + 1)
+            test_log_loss.write(line_avg)
+            test_log_acc.write(line_acc)
 
             if test_accuracy > best_acc:
                 best_acc = test_accuracy
@@ -277,30 +308,13 @@ def main():
 
     # --- close log files ---#
     result_file.close()
-
-    if args.tensorboard:
-        writer_train.close()
-        writer_val.close()
-
-
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
+    train_log_loss.close()
+    train_log_acc.close()
+    test_log_acc.close()
+    train_log_loss_batch.close()
+    test_log_loss.close()
+    writer.export_scalars_to_json(path_exp + "/all_scalars.json")
+    writer.close()
 
 if __name__ == '__main__':
     main()
